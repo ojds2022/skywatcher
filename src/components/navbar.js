324f7524573135a8fdd02454b6ef0aaa3d8db0bf
ojds2022 <odiazstrandberg@gmail.com>
2023-06-01 09:27:57 -0700

    refactor: navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Brightness4SharpIcon from '@mui/icons-material/Brightness4Sharp';
 import '../styles/navbar.css';
 
@@ -12,27 +13,31 @@ function Navbar() {
   }
 
   return (
-    <div className="navbar w-full h-14 bg-light-purple bg-opacity-70 absolute top-0 left-0 flex flex-row z-20">
-      {openLinks ? 
-          <div className="flex items-center justify-between px-6 grow">
-              <Link to="/"><span className='text-xl text-white hover:text-pale-green'>Home</span></Link>
-              <Link to="/about"><span className='text-xl text-white hover:text-pale-green'>About</span></Link>
-              <Link to="/contact"><span className='text-xl text-white hover:text-pale-green'>Contact</span></Link>
-              <button className='text-white hover:text-pale-green bg-transparent border-0 cursor-pointer burger' onClick={toggleNavbar}>
-                  <ReorderIcon />
-              </button>
-          </div>
-      :
-          <div className="flex items-center justify-between px-6 grow">
-              <Link to="/">
-                  <span className='text-white hover:text-pale-green cursor-pointer pt-1 navbarLogo'><Brightness4SharpIcon /></span>
-              </Link>
-              <button className='text-white hover:text-pale-green bg-transparent border-0 cursor-pointer burger' onClick={toggleNavbar}>
-                  <ReorderIcon />
-              </button>
-          </div>
-      }
-    </div>
+    <>
+        {openLinks ?
+            <div className='absolute top-0 right-0 z-20 w-1/3 md:w-1/5 h-2/5 navbar bg-light-purple bg-opacity-90'>
+                <div className="flex flex-col items-center mt-5 grow gap-y-5 3xl:gap-y-16">
+                    <button id="arrowUp" className='bg-transparent border-0 cursor-pointer' onClick={toggleNavbar}>
+                        <KeyboardArrowUpIcon />
+                    </button>
+                    <Link to="/skywatcher"><span className='text-xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>Home</span></Link>
+                    <Link to="/about"><span className='text-xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>About</span></Link>
+                    <Link to="/"><span className='text-xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>Portfolio</span></Link>
+                </div>
+            </div>
+          :
+            <div className="absolute top-0 left-0 z-20 flex flex-row w-full navbar h-14 xl:h-20 3xl:h-32 bg-light-purple bg-opacity-70">
+                <div className="flex items-center justify-between px-6 grow">
+                    <Link to="/skywatcher">
+                        <span id="navbarLogo" className='pt-1 text-white cursor-pointer hover:text-pale-green'><Brightness4SharpIcon /></span>
+                    </Link>
+                    <button id="burger" className='bg-transparent border-0 cursor-pointer' onClick={toggleNavbar}>
+                        <ReorderIcon />
+                    </button>
+                </div>
+            </div>
+        }
+    </>
   );
 }
 
