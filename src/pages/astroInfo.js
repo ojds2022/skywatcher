e6bb1eded2707c4525e964245309471c3fb2a69b
ojds2022 <odiazstrandberg@gmail.com>
2023-06-05 09:05:57 -0700

    feat: added id to search button

--- src/pages/astroInfo.js
+++ src/pages/astroInfo.js
@@ -78,7 +78,7 @@ const AstroInfo = () => {
                 <div className="px-2 pb-4 text-center bg-turquoise bg-opacity-40 rounded-xl 3xl:rounded-2xl xl:py-3 3xl:py-8">
                       <h2 className='mb-5 text-xl font-bold text-white md:text-2xl xl:text-4xl xl:mb-12 3xl:text-6xl'>Enter your city or zip-code:</h2>
                       <input id="inputField" className='focus:outline-none md:w-44 md:h-7 xl:w-60 xl:h-10 xl:text-lg 3xl:w-80 3xl:h-16 3xl:text-3xl' type="text" placeholder="Enter here..."/>
-                      <button className='px-1 ml-2 text-white rounded hover:bg-pale-green bg-light-purple hover:text-black md:h-7 md:w-16 xl:h-10 xl:w-20 xl:text-lg xl:ml-5 3xl:h-16 3xl:w-32 3xl:rounded-xl' type="submit" onClick={fetchAstronomyInfo}>Search</button>
+                      <button id="searchButton" className='px-1 ml-2 text-white rounded hover:bg-pale-green bg-light-purple hover:text-black md:h-7 md:w-16 xl:h-10 xl:w-20 xl:text-lg xl:ml-5 3xl:h-16 3xl:w-32 3xl:rounded-xl' type="submit" onClick={fetchAstronomyInfo}>Search</button>
                 </div>
             }
         </div>
