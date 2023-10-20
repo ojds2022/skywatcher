a6db0f684446b3635d532082798d53529bae172b
ojds2022 <odiazstrandberg@gmail.com>
2023-10-20 15:59:52 -0700

    feat: buger pop-up menu in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Home from '../pages/home';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import CloseIcon from '@mui/icons-material/Close';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Brightness4SharpIcon from '@mui/icons-material/Brightness4Sharp';
 import { AstroInfo } from '../pages/astroInfo';
@@ -19,17 +20,21 @@ function Footer() {
     <>
         {openLinks ?
             <div id='buger-open' className="">
-                <div className='relative -right-5 top-10'>
+                <div className='relative top-28 -right-12'>
                   <button id="burger-open" className='bg-transparent border-0 cursor-pointer' onClick={toggleNavbar}>
-                      <MenuOpenIcon />
+                      <CloseIcon />
                   </button>
                 </div>
-                <div className='relative z-50 flex flex-col rounded-md -right-5 -top-24 bg-turquoise opacity-70'>
-                    <Link to="/">
-                      <span className='text-xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>Home</span>
+                <div className='relative z-50 flex flex-col rounded-md -right-5 -top-40 bg-turquoise opacity-70'>
+                    <Link className='p-5' to="/">
+                      <span className='text-2xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>Home</span>
                     </Link>
-                    <Link to="/about"><span className='text-xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>About</span></Link>
-                    <Link to="/"><span className='text-xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>Portfolio</span></Link>
+                    <Link className='p-5' to="/about">
+                      <span className='text-2xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>About</span>
+                    </Link>
+                    <Link className='p-5' to="/">
+                      <span className='text-2xl text-white md:text-2xl xl:text-3xl 3xl:text-6xl hover:text-pale-green'>Portfolio</span>
+                    </Link>
                 </div>
             </div>
         
